3cc7dd1ec5c59904b01685e6474e735e636cc524
jsc5023 <kir5121@naver.com>
2024-04-30 22:03:06 +0900

    Jeklly를 사용한 깃허브 블로그 만들기

---
+++ README.md
@@ -0,0 +1,6 @@
+Jekyll를 이용하여 블로그를 만드는 리포지터리입니다.
+
+---
+참조 Jeklly Github 주소 : https://github.com/jekyll/jekyll
+<br/>
+theme 참조 Github : cotes2020/jekyll-theme-chirpy
---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1714481431',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],allowHosts: [
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'cdnjs.cloudflare.com'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
