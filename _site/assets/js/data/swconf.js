e7253ad9c028ed141c6f98fce03453c17a244b6c
SeongCheol Jung <kir5121@naver.com>
2024-12-22 08:47:55 +0900

    [docs] - 메인화면을 나에게 맞는 형태로 변경

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734822121',resources: [
+    cacheName: 'chirpy-1734824781',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
