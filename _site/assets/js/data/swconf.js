d5d4b1f78dc85c7c6a3db254559378f57e127689
SeongCheol Jung <kir5121@naver.com>
2024-12-22 02:13:35 +0900

    [docs] - _config파일 변경

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734799643',resources: [
+    cacheName: 'chirpy-1734801071',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
