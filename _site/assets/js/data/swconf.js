81e52ae138b88865a582f284bf1b3e5def0aaea6
SeongCheol Jung <kir5121@naver.com>
2024-12-22 08:07:58 +0900

    [docs] - 기본적인 정보 복구

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734805410',resources: [
+    cacheName: 'chirpy-1734822121',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
