3a61bfc6803506a193d19596d4e82ce015fc1bc1
SeongCheol Jung <kir5121@naver.com>
2025-01-06 00:49:26 +0900

    왼쪽 사진 및 그림이 너무 치우쳐져 있어서 변경하였습니다.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734827236',resources: [
+    cacheName: 'chirpy-1736092084',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
