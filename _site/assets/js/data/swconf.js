1db11ca7dd18e9d479abe2966f3ba29df14a5495
SeongCheol Jung <kir5121@naver.com>
2025-01-06 17:07:50 +0900

    정적 유틸리티 클래스 이해하기

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736092676',resources: [
+    cacheName: 'chirpy-1736150785',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
